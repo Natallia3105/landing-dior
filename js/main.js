6b96d9cb84609ed51dc95c151aaa33f0d11d210a
natalagoncaruk <honcharukna@gmail.com>
2024-07-09 21:05:05 +0200

    continue adaptability of the page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const swiperTop = new Swiper('.top__swiper', {
     // Optional parameters
     effect: 'fade',
-    autoplay: {
-        delay: 3500,
-        disableOnInteraction: false,
-    },
+    // autoplay: {
+    //     delay: 3500,
+    //     disableOnInteraction: false,
+    // },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
